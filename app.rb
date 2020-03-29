d64c4ae5414eacfdedbdd76615001a11ceb86905
dsuccar <danielsuccar@icloud.com>
2020-03-29 18:18:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Danny"
+  end
+  get '/hometown' do
+    "My hometown is Rockville, MD"
+  end
+  get '/favorite-song' do
+    "My favorite song is Runaway"
+  end
 end
