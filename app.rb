1e507845fb6142c686f9d681242e1aafb63f3910
bret-gibson <bret.gibson@gmail.com>
2020-03-30 07:57:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "hello"
+    end
+
+    get '/name' do
+        "My name is Bret Gibson"
+    end
+
+    get '/hometown' do
+        "My hometown is Washington, D.C"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is I dont have a favorite :)"
+    end
+    
 end
